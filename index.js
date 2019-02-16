97e6a8e445fe09158221ca82c16a4f8858be58cf
L D <tapircode@gmail.com>
2019-02-16 16:32:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,3 +20,8 @@ function destructivelyRemoveFirstKitten(name){
   kittens.shift(name);
   return kittens;
 }
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name);
+  returns kittens;
+}
